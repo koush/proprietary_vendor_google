9cb85ea11dee20825bf4e2581e23939b2134a1d8
DrMacinyasha <drmacinyasha@gmail.com>
2011-03-14 16:17:37 -0700

    Initial offerings

---
+++ manifest.js
@@ -0,0 +1,86 @@
+{
+    version: 1,
+    homepage: "http://goo-inside.me/gapps/",
+    donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KNWA22ZGW32VS",
+    
+    // Your list of ROMs
+    roms:
+    [
+    {
+        name: "Google Apps 20110307 Universal",
+        summary: "March 7, 2011 for Android 2.3.3 (CyanogenMod 7 Nightly 11+, RC2, RC3, 7.0.0 Stable)",
+        device: 
+        [
+            blade",
+            "bravo",
+            "bravoc",
+            "buzz",
+            "click",
+            "crespo",
+            "desirec",
+            "dream_sapphire",
+            "encore",
+            "epic",
+            "espresso",
+            "glacier",
+            "harmony",
+            "hero",
+            "heroc",
+            "inc",
+            "legend",
+            "leo",
+            "liberty",
+            "one",
+            "passion",
+            "sholes",
+            "streak",
+            "supersonic",
+            "vega",
+            "vibrant",
+            "vision",
+            "z71",
+            "zero"
+        ],
+        url: "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"
+    },
+    
+    {
+        name: "Google Apps 20110120 Universal",
+        summary: "January 20, 2011 for Android 2.3.2 (CyanogenMod Nightly <11, RC1)",
+        device: 
+        [
+            blade",
+            "bravo",
+            "bravoc",
+            "buzz",
+            "click",
+            "crespo",
+            "desirec",
+            "dream_sapphire",
+            "encore",
+            "epic",
+            "espresso",
+            "glacier",
+            "harmony",
+            "hero",
+            "heroc",
+            "inc",
+            "legend",
+            "leo",
+            "liberty",
+            "one",
+            "passion",
+            "sholes",
+            "streak",
+            "supersonic",
+            "vega",
+            "vibrant",
+            "vision",
+            "z71",
+            "zero"
+        ],
+        url: "http://goo-inside.me/gapps/gapps-gb-20110120-signed.zip"
+    }
+
+    ]
+}
