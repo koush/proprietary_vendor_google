cb880a4f52843d5e5c81ba9fc57393d848c8b66d
Denham Crafton <drmacinyasha@gmail.com>
2011-03-15 13:29:55 -0700

    Add CyanogenMod 4 and 7 GApps packs.

--- manifest.js
+++ manifest.js
@@ -5,9 +5,10 @@
     
     roms:
     [
+	// CyanogenMod 7
     {
-        name: "Google Apps 20110307 Universal",
-        summary: "March 7, 2011 for Android 2.3.3 (CyanogenMod 7 Nightly 11+, RC2, RC3, 7.0.0 Stable)",
+        name: "Google Apps 20110307 Universal (RECOMMENDED)",
+        summary: "March 7, 2011 for Android 2.3.3 (CyanogenMod 7 Nightly 11+, RC2-3, Stable)",
         device: 
         [
             "blade",
@@ -42,10 +43,9 @@
         ],
         url: "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"
     },
-    
     {
         name: "Google Apps 20110120 Universal",
-        summary: "January 20, 2011 for Android 2.3.2 (CyanogenMod Nightly <11, RC1)",
+        summary: "January 20, 2011 for Android 2.3.2 (CyanogenMod 7 Nightly <11, RC1)",
         device: 
         [
             "blade",
@@ -79,7 +79,103 @@
             "zero"
         ],
         url: "http://goo-inside.me/gapps/gapps-gb-20110120-signed.zip"
-    }
-
+    },
+    {
+        name: "Google Apps 20110118 Tegra",
+        summary: "January 18, 2011 for Android 2.3.2 Tegra devices",
+        device: "harmony",
+        url: "http://goo-inside.me/gapps/gapps-gb-tegra-20110118-signed.zip"
+    },
+    {
+        name: "Google Apps 20110115 Universal",
+        summary: "January 15, 2011 for Android 2.3.2",
+        device: 
+        [
+            "blade",
+            "bravo",
+            "bravoc",
+            "buzz",
+            "click",
+            "crespo",
+            "desirec",
+            "dream_sapphire",
+            "encore",
+            "epic",
+            "espresso",
+            "glacier",
+            "harmony",
+            "hero",
+            "heroc",
+            "inc",
+            "legend",
+            "leo",
+            "liberty",
+            "one",
+            "passion",
+            "sholes",
+            "streak",
+            "supersonic",
+            "vega",
+            "vibrant",
+            "vision",
+            "z71",
+            "zero"
+        ],
+        url: "http://goo-inside.me/gapps/gapps-gb-20110115-signed.zip"
+    },
+    {
+        name: "Google Apps 20110114 Universal",
+        summary: "January 14, 2011 for Android 2.3.2",
+        device: 
+        [
+            "blade",
+            "bravo",
+            "bravoc",
+            "buzz",
+            "click",
+            "crespo",
+            "desirec",
+            "dream_sapphire",
+            "encore",
+            "epic",
+            "espresso",
+            "glacier",
+            "harmony",
+            "hero",
+            "heroc",
+            "inc",
+            "legend",
+            "leo",
+            "liberty",
+            "one",
+            "passion",
+            "sholes",
+            "streak",
+            "supersonic",
+            "vega",
+            "vibrant",
+            "vision",
+            "z71",
+            "zero"
+        ],
+        url: "http://goo-inside.me/gapps/gapps-gb-20110114-signed.zip"
+    },
+	{
+        name: "Google Apps 20110113 Tegra",
+        summary: "January 13, 2011 for Android 2.3.2 Tegra devices",
+        device: "harmony",
+        url: "http://goo-inside.me/gapps/gapps-gb-tegra-20110113-signed.zip"
+    },
+	// CyanogenMod 6
+	
+	// CyanogenMod 5
+	
+	// CyanogenMod 4
+	{
+        name: "Google Apps DRC83",
+        summary: "For Android 1.6 (CyanogenMod 4)",
+        device: "dream_sapphire",
+        url: "http://goo-inside.me/gapps/DRC83_base_defanged.zip"
+    },
     ]
 }
